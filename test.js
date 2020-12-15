3edf42e5ddc1628c05f96d7e8edb21fa89ba8f3c
Raxel21 <yisusparley@gmail.com>
2020-12-15 03:13:41 -0600

    Initial commit

---
+++ test.js
@@ -0,0 +1,10 @@
+for(let i = 1; i <= 10; i++) {
+	// console.log(i);
+	for(let j = 1; j <= 10; j++){
+		// console.log(i * j);
+		console.log(`${j} * ${i} = ${j * i}`);
+	}
+
+	console.log('\t');
+}
+
